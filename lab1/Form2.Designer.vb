dc56e96a1a536ccec8abcaada742295246d28381
jiratsa <jiratsa.sirichan@gmail.com>
2018-12-04 11:36:46 +0700

    04/12/2561

---
+++ lab1/Form2.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtComputer = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPrinter = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblCommissions1 = New System.Windows.Forms.Label()
+        Me.lblCommissionstotal = New System.Windows.Forms.Label()
+        Me.btncompute = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.lblCommissions2 = New System.Windows.Forms.Label()
+        Me.lblsale2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblsale1 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblsaletotal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtComputer
+        '
+        Me.txtComputer.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtComputer.Location = New System.Drawing.Point(276, 46)
+        Me.txtComputer.Name = "txtComputer"
+        Me.txtComputer.Size = New System.Drawing.Size(112, 20)
+        Me.txtComputer.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(99, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(146, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Computer Acer Veriton x5"
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(99, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(133, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Printer HP M529DM"
+        '
+        'txtPrinter
+        '
+        Me.txtPrinter.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtPrinter.Location = New System.Drawing.Point(276, 87)
+        Me.txtPrinter.Name = "txtPrinter"
+        Me.txtPrinter.Size = New System.Drawing.Size(112, 20)
+        Me.txtPrinter.TabIndex = 1
+        '
+        'Label5
+        '
+        Me.Label5.Location = New System.Drawing.Point(424, 152)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(133, 20)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "ค่าคอมมิชชั่น 5%"
+        '
+        'Label6
+        '
+        Me.Label6.Location = New System.Drawing.Point(424, 185)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(133, 20)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "ค่าคอมมิชชั่น 10%"
+        '
+        'Label7
+        '
+        Me.Label7.Location = New System.Drawing.Point(424, 220)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(133, 20)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "ค่าคอมมิชชั่นรวม"
+        '
+        'lblCommissions1
+        '
+        Me.lblCommissions1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblCommissions1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblCommissions1.Location = New System.Drawing.Point(544, 147)
+        Me.lblCommissions1.Name = "lblCommissions1"
+        Me.lblCommissions1.Size = New System.Drawing.Size(115, 23)
+        Me.lblCommissions1.TabIndex = 2
+        '
+        'lblCommissionstotal
+        '
+        Me.lblCommissionstotal.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblCommissionstotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblCommissionstotal.Location = New System.Drawing.Point(544, 219)
+        Me.lblCommissionstotal.Name = "lblCommissionstotal"
+        Me.lblCommissionstotal.Size = New System.Drawing.Size(115, 23)
+        Me.lblCommissionstotal.TabIndex = 2
+        '
+        'btncompute
+        '
+        Me.btncompute.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btncompute.Location = New System.Drawing.Point(343, 266)
+        Me.btncompute.Name = "btncompute"
+        Me.btncompute.Size = New System.Drawing.Size(86, 31)
+        Me.btncompute.TabIndex = 3
+        Me.btncompute.Text = "คำนวณ"
+        Me.btncompute.UseVisualStyleBackColor = False
+        '
+        'Label9
+        '
+        Me.Label9.Location = New System.Drawing.Point(85, 220)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(133, 20)
+        Me.Label9.TabIndex = 1
+        Me.Label9.Text = "ยอดขายรวม"
+        '
+        'lblCommissions2
+        '
+        Me.lblCommissions2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblCommissions2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblCommissions2.Location = New System.Drawing.Point(544, 182)
+        Me.lblCommissions2.Name = "lblCommissions2"
+        Me.lblCommissions2.Size = New System.Drawing.Size(115, 23)
+        Me.lblCommissions2.TabIndex = 2
+        '
+        'lblsale2
+        '
+        Me.lblsale2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblsale2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblsale2.Location = New System.Drawing.Point(273, 184)
+        Me.lblsale2.Name = "lblsale2"
+        Me.lblsale2.Size = New System.Drawing.Size(115, 23)
+        Me.lblsale2.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(85, 150)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(133, 20)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "ยอดขายสินค้า 1"
+        '
+        'lblsale1
+        '
+        Me.lblsale1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblsale1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblsale1.Location = New System.Drawing.Point(273, 149)
+        Me.lblsale1.Name = "lblsale1"
+        Me.lblsale1.Size = New System.Drawing.Size(115, 23)
+        Me.lblsale1.TabIndex = 2
+        '
+        'Label8
+        '
+        Me.Label8.Location = New System.Drawing.Point(85, 187)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(133, 20)
+        Me.Label8.TabIndex = 1
+        Me.Label8.Text = "ยอดขายสินค้า 2"
+        '
+        'lblsaletotal
+        '
+        Me.lblsaletotal.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblsaletotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblsaletotal.Location = New System.Drawing.Point(273, 217)
+        Me.lblsaletotal.Name = "lblsaletotal"
+        Me.lblsaletotal.Size = New System.Drawing.Size(115, 23)
+        Me.lblsaletotal.TabIndex = 2
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(690, 322)
+        Me.Controls.Add(Me.btncompute)
+        Me.Controls.Add(Me.lblCommissionstotal)
+        Me.Controls.Add(Me.lblCommissions2)
+        Me.Controls.Add(Me.lblCommissions1)
+        Me.Controls.Add(Me.lblsale1)
+        Me.Controls.Add(Me.lblsaletotal)
+        Me.Controls.Add(Me.lblsale2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPrinter)
+        Me.Controls.Add(Me.txtComputer)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtComputer As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPrinter As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblCommissions1 As Label
+    Friend WithEvents lblCommissionstotal As Label
+    Friend WithEvents btncompute As Button
+    Friend WithEvents Label9 As Label
+    Friend WithEvents lblCommissions2 As Label
+    Friend WithEvents lblsale2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblsale1 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lblsaletotal As Label
+End Class
---
+++ lab1/Form2.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form2
+    Private Sub btncompute_Click(sender As Object, e As EventArgs) Handles btncompute.Click
+
+
+        Dim sale1 As Double
+        Dim sale2 As Double
+        Dim saletotal As Double
+        Dim Commissions1 As Double
+        Dim Commissions2 As Double
+        Dim Commissionstotal As Double
+
+        sale1 = Val(txtComputer.Text)
+        sale2 = Val(txtPrinter.Text)
+        saletotal = sale1 + sale2
+
+        Commissions1 = sale1 * 0.05
+        Commissions2 = sale2 * 0.1
+        Commissionstotal = Commissions1 + Commissions2
+
+        lblsale1.Text = sale1
+        lblsale2.Text = sale2
+        lblsaletotal.Text = saletotal
+        lblCommissions1.Text = Commissions1
+        lblCommissions2.Text = Commissions2
+        lblCommissionstotal.Text = Commissionstotal
+
+
+
+
+
+
+
+
+    End Sub
+End Class
